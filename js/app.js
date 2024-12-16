d2d7833de1e7e03444dd60dc241b5294a59b097a
Reza <rezar002024@gmail.com>
2024-12-16 10:22:35 +0330

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// typed animation
+
+var typed = new typed(".typing", {
+  strings: ["", "Front-end Developer", "Web Developer", "Graphic Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
